b787e265dea22c0a13ec30285bb6b840e496fc57
Thomas Nowak <tnowak@plex.com>
2017-09-25 16:59:43 -0400

    Add info

--- start.js
+++ start.js
@@ -33,26 +33,33 @@ Client.on('guildCreate', guild => {
     "default_role": ""
   };
 
+  var guildRecord = {
+    "guildID": guild.id,
+    "guildInformation": guildInformation
+  };
+
   console.log(`\nNew guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
 
-  Guild.guildInformationUpsert(guild, guildInformation);
+  Guild.guildUpsert(guildRecord);
 });
 
 Client.on('guildDelete', guild => {
   console.log(`\nGuild Removed: ${guild.name} (id: ${guild.id}). This guild had ${guild.memberCount} members!`);
 
-  Guild.guildInformationDelete(guild);
+ // Guild.guildInformationDelete(guild);
 });
 
 Client.on("guildMemberAdd", member => {
-  var channel = member.guild.channels.find(x => x.name === "landing_achannel");
-
-  channel.send(`Welcome ${member.user} to the ${member.guild.name} discord server.  Type !auth to authenticate with your EVE Online account and continue.`);
+  member.send(`Welcome ${member.user} to the ${member.guild.name} discord server.  Type **!auth** in a channel in the server with me in it to authenticate with your EVE Online account, or try **!help**.`);
 });
 
 Client.on('message', msg => {
   if(msg.author.bot) return;
   if(msg.content.indexOf(Config.prefix) !== 0) return;
+  if(!msg.guild) {
+    msg.reply(`Please converse with me in a guild channel instead.`);
+    return;
+  }
 
   var args = msg.content.slice(Config.prefix.length).trim().split(/ +/g);
   var command = args.shift().toLowerCase();
